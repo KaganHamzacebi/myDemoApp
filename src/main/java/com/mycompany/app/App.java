c1660b47ee598dc558b0a62f4b3df725d75cc594
KaganHamzacebi <kagan.hcebi@gmail.com>
2019-10-05 21:35:41 +0300

    try

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -31,7 +31,7 @@ public class App
     public static void main( String[] args ) {
         port(getHerokuAssignedPort());
 
-        get("/",(req,res) -> "Hello, World");
+        get("/",(req,res) -> search(inputList,value));
 
         post("/compute",(req,res) -> {
 			//System.out.println(req.queryParams("input1"));
